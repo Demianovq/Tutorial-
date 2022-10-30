f24fa23104bef67271cfacb0a8029f28512db0a7
Demianovq <110334010+Demianovq@users.noreply.github.com>
2022-10-30 16:29:01 +0200

    adds

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function checkStorage(available, ordered) {
+  let message;
+  if ((ordered) => available) {
+    message = "Not enough goods in stock!";
+  } else {
+    message = "Order is processed, our manager will contact you.";
+  }
+  return message;
+}
